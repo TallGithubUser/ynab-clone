549deacadae6d90fb0336d8b064b52522c1df51f
Luke Simon <lukesimon@Lukes-MacBook-Pro.local>
2023-09-03 12:20:02 -0500

    Add uniqueness check to anything with _id

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,4 +4,6 @@ class Transaction < ApplicationRecord
 	belongs_to :category
 	belongs_to :budget
 	validates :uid, presence: true, uniqueness: true
+	validates :import_id, uniqueness: true
+	validates :matched_transaction_id, uniqueness: true
 end
