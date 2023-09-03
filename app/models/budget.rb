d850b911aa7b4b6fa29425e6eac54fa1b4228261
Luke Simon <lukesimon@Lukes-MacBook-Pro.local>
2023-09-03 11:37:13 -0500

    Add belongs_to and has_many connection between Budget/Payee and Budget/CategoryGroup

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,6 @@
 class Budget < ApplicationRecord
 	has_many :accounts
+	has_many :category_groups
+	has_many :payees
 	validates :uid, presence: true, uniqueness: true
 end
--- app/models/category_group.rb
+++ app/models/category_group.rb
@@ -1,4 +1,5 @@
 class CategoryGroup < ApplicationRecord
 	has_many :categories
+	belongs_to :budget
 	validates :uid, uniqueness: true, presence: true
 end
