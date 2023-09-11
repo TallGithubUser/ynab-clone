8bc2aca111acbde623598c2b8cacd18b3bcb4f31
Luke Simon <TallGithubUser>
2023-09-10 21:58:30 -0500

    Add rudimentary method for adding new transactions

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -22,7 +22,7 @@ class AccountController < ApplicationController
 			end
 
 			t = {
-			:account_name => transaction.account.name,
+				:account_name => transaction.account.name,
 				:date => transaction.date, 
 				:payee => transaction.payee.name, 
 				:category_group => category_group, 
@@ -39,6 +39,14 @@ class AccountController < ApplicationController
 	def index
 		budget = Budget.find_by(uid:params[:budget_id])
 		last_100_transactions = budget.transactions.reverse[0..100]
+		last_20_payees = Payee.all.reverse[0..20]
+
+		open_accounts = Account.all.select{|account| account.budget == budget and account.closed == false}
+		available_categories = Category.all.reject{|category| category.category_group.budget != budget}
+
+		@categories_with_uid = available_categories.map{|category| [category.name, category.uid]}
+		@payees_with_uid = last_20_payees.map{|payee| [payee.name, payee.uid]}
+		@accounts_with_uid = open_accounts.map{|account| [account.name, account.uid]}
 		@accounts = budget.accounts.order(:name)
 		@transactions = sanitize_transactions(last_100_transactions)
 	end
@@ -49,16 +57,26 @@ class AccountController < ApplicationController
 	end
 
 	def new
-		@article = Article.new
+		@new_transaction = Transaction.new
 	end
 
 	def create
-    	@article = Article.new(title: "...", body: "...")
-
-	    if @article.save
-	      redirect_to @article
-	    else
-	      render :new, status: :unprocessable_entity
-	    end
+		t = Transaction.new(
+			uid: SecureRandom.uuid,
+	  		date: Date.today,
+	  		amount: (params[:amount].to_f*1000).floor,
+	  		memo: params[:memo],
+	  		cleared: params[:cleared] == '1' ? "cleared" : "uncleared",
+	  		approved: true,
+	  		deleted: false,
+	  		payee: Payee.find_by(uid: params[:payee]),
+	  		category: Category.find_by(uid: params["category"]),
+	  		account: Account.find_by(uid: params["account"]),
+	  		budget: Budget.find_by(uid: params["budget_id"])
+	  	)
+	  	
+	  	if t.save
+			redirect_to "/#{params["budget_id"]}/account"	
+		end
   	end
 end
